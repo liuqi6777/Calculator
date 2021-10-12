d0267d258dd5ba022ba3a0da751acf5b62f66979
liuqi6777 <3169780038@qq.com>
2021-10-12 22:52:08 +0800

    update .gitignore

--- build/CMakeFiles/defines.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for defines.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/qianx/Desktop/Calculator
-# Build directory: C:/Users/qianx/Desktop/Calculator/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
